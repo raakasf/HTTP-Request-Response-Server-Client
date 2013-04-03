87be598bd2e3a4949e09a4f85077d7b7527cd9a7
Rakesh Sehgal <rakeshsehgal@Rakeshs-MacBook-Pro.local>
2013-04-03 01:14:11 -0400

    Initial Commit

---
+++ Learning/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Learning
+//
+//  Created by Rakesh Sehgal on 4/3/13.
+//  Copyright (c) 2013 Rakesh Sehgal. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
